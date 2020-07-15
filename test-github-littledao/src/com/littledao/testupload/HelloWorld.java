496841d2b589e6e1165dd016cc408371cb44e99c
sun <272328946@qq.com>
2020-07-15 11:54:38 +0800

    initializing repository

---
+++ test-github-littledao/src/com/littledao/testupload/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.littledao.testupload;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("这是一个github测试上传项目");
+	}
+
+}
